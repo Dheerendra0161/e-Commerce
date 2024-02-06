32d0e92da5d744be0fd92bc505277cd072b6d489
Dheerendra <dheerendra0161@gmail.com>
2024-02-06 15:09:22 +0530

    gitignore and first commit

---
+++ src/main/java/eCom/Utils/KnowSystem.java
@@ -0,0 +1,10 @@
+package eCom.Utils;
+
+public class KnowSystem {
+
+	public static void main(String[] args) {
+			System.getProperties().list(System.out);
+			//Just run the code and see the console output
+	}
+
+}
---
+++ src/test/java/eCom/Testcases/SearchTest.java
@@ -0,0 +1,69 @@
+package eCom.Testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import eCom.Base.Base;
+import eCom.Pages.HomePage;
+import eCom.Pages.SearchPage;
+
+//Updated Comment - Added more details
+
+public class SearchTest extends Base {
+
+	SearchPage searchPage;
+	HomePage homePage;
+
+	public SearchTest() {
+		super();
+	}
+
+	public WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+
+		driver = initializeBrowserAndOpenApplicationURL(prop.getProperty("browserName"));
+		homePage = new HomePage(driver);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+
+		driver.quit();
+
+	}
+
+	@Test(priority = 1)
+	public void verifySearchWithValidProduct() {
+
+		searchPage = homePage.searchForAProduct(dataProp.getProperty("validProduct"));
+		Assert.assertTrue(searchPage.displayStatusOfHPValidProduct(),
+				"Valid product HP is not displayed in the search results");
+
+	}
+
+	@Test(priority = 2)
+	public void verifySearchWithInvalidProduct() {
+
+		searchPage = homePage.searchForAProduct(dataProp.getProperty("invalidProduct"));
+		Assert.assertEquals(searchPage.retrieveNoProductMessageText(),
+				"There is no product that matches the search criteria.",
+				"No product message in search results is not displayed");
+
+	}
+
+	@Test(priority = 3, dependsOnMethods = { "verifySearchWithValidProduct" })
+	public void verifySearchWithoutAnyProduct() {
+
+		searchPage = homePage.clickOnSearchButton();
+		Assert.assertEquals(searchPage.retrieveNoProductMessageText(),
+				dataProp.getProperty("NoProductTextInSearchResults"),
+				"No product message in search results is not displayed");
+
+	}
+
+}
