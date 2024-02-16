9bbfa6fcd66e42b8fdf176595062dce749b3afa4
Dheerendra <dheerendra0161@gmail.com>
2024-02-16 10:29:40 +0530

    Testing Utils

--- src/main/java/eCom/Utils/ActionsUtils.java
+++ src/main/java/eCom/Utils/ActionsUtils.java
@@ -6,41 +6,46 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class ActionsUtils {
-	private WebDriver driver;
-	private Actions actions;
 
-	public ActionsUtils(WebDriver driver) {
-		this.driver = driver;
-		this.actions = new Actions(driver);
-	}
+    // Click on an element
+    public static void clickElement(WebDriver driver, WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.click(element).build().perform();
+    }
 
-	// Click on an element
-	public void clickElement(WebElement element) {
-		actions.click(element).build().perform();
-	}
+    // Double-click on an element
+    public static void doubleClickElement(WebDriver driver, WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.doubleClick(element).build().perform();
+    }
 
-	// Double-click on an element
-	public void doubleClickElement(WebElement element) {
-		actions.doubleClick(element).build().perform();
-	}
+    // Right-click on an element
+    public static void rightClickElement(WebDriver driver, WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.contextClick(element).build().perform();
+    }
 
-	// Right-click on an element
-	public void rightClickElement(WebElement element) {
-		actions.contextClick(element).build().perform();
-	}
+    // Drag and drop an element
+    public static void dragAndDrop(WebDriver driver, WebElement source, WebElement target) {
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(source, target).build().perform();
+    }
 
-	// Drag and drop an element
-	public void dragAndDrop(WebElement source, WebElement target) {
-		actions.dragAndDrop(source, target).build().perform();
-	}
+    // Simulate a chord action, e.g., Ctrl + A (Select All)
+    public static void selectAll(WebDriver driver) {
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build().perform();
+    }
+    
+    // Simulate a control-click action on an element
+    public static void controlClickOnElement(WebDriver driver, WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.CONTROL).click(element).keyUp(Keys.CONTROL).perform(); 
+    }
 
-	// Simulate a chord action, e.g., Ctrl + A (Select All)
-	public void selectAll() {
-		actions.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build().perform();
-	}
-
-	// Send keys to an element
-	public void sendKeysToElement(WebElement element, CharSequence... keysToSend) {
-		actions.sendKeys(element, keysToSend).build().perform();
-	}
+    // Send keys to an element
+    public static void sendKeysToElement(WebDriver driver, WebElement element, CharSequence... keysToSend) {
+        Actions actions = new Actions(driver);
+        actions.sendKeys(element, keysToSend).build().perform();
+    }
 }
